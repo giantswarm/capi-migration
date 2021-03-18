b1de22d3519066d20466c14268d20f643070e520
Paweł Kopiczko <pawel@giantswarm.io>
2021-03-18 10:03:15 +0000

    Create cluster controller (#4)
    Commands I used:
    
    ```
    kubebuilder create api \
    	--controller=true \
    	--resource=false \
    	--group=cluster.x-k8s.io \
    	--version=v1alpha3 \
    	--kind=Cluster \
    	--make=false
    
    mkdir -p config/crd
    touch config/crd/kustomization.yaml
    
    devctl replace -i \
    	'github.com/giantswarm/capi-migration/api/v1alpha3' \
    	'sigs.k8s.io/cluster-api/api/v1alpha3' \
    	'**/*.go'
    devctl replace -i \
    	'clusterxk8siov1alpha3' \
    	'capiv1alpha3' \
    	'**/*.go'
    
    go get github.com/go-logr/logr@v0.1.0 # This dependency will be replaced anyway.
    go get sigs.k8s.io/cluster-api@v0.3.13
    go mod tidy
    ```
    
    Binaries versions:
    
    ```
    $ go version
    go version go1.16 darwin/amd64
    
    $ kubebuilder version
    Version: version.Version{KubeBuilderVersion:"2.3.2", KubernetesVendor:"1.16.4", GitCommit:"5da27b892ae310e875c8719d94a5a04302c597d0", BuildDate:"2021-02-16T19:31:47Z", GoOs:"unknown", GoArch:"unknown"}
    
    $ devctl version
    Version:		4.2.2-dev
    Git Commit:	 5d1d3543ae809367e7ccb19efea946f14c4b98e7
    Go Version:	 go1.15.6
    OS / Arch:	  darwin / amd64
    Source:		 https://github.com/giantswarm/devctl
    ```

--- main.go
+++ main.go
@@ -25,6 +25,9 @@ import (
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	"github.com/giantswarm/capi-migration/controllers"
+	capiv1alpha3 "sigs.k8s.io/cluster-api/api/v1alpha3"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -36,6 +39,7 @@ var (
 func init() {
 	_ = clientgoscheme.AddToScheme(scheme)
 
+	_ = capiv1alpha3.AddToScheme(scheme)
 	// +kubebuilder:scaffold:scheme
 }
 
@@ -62,6 +66,14 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controllers.ClusterReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("Cluster"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Cluster")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
