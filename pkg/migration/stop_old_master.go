5662e08f433ec1a85acd8e3e6ec9adbc2b0dca21
Christian Bianchi <whites11@gmail.com>
2021-03-23 10:21:53 +0100

    Make pod creation more resilient

--- pkg/migration/stop_old_master.go
+++ pkg/migration/stop_old_master.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/giantswarm/microerror"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
@@ -17,64 +18,82 @@ func (m *azureMigrator) stopOldMasterComponents(ctx context.Context) error {
 		return microerror.Mask(err)
 	}
 
+	// Using the kube-proxy service account because I am sure it exists and it can use the privileged PSP.
+	serviceAccountName := "kube-proxy"
+	podNamespace := "kube-system"
 	m.logger.Debugf(ctx, "found %d legacy nodes", len(nodeNames))
 
 	for _, nodeName := range nodeNames {
 		podName := fmt.Sprintf("disable-master-node-components-%s", nodeName)
-		podNamespace := "default"
 
-		// Create pod definition.
-		var pod corev1.Pod
-		{
-			command := `
+		// Check if pod already exists.
+		existing := corev1.Pod{}
+		err = m.wcCtrlClient.Get(ctx, client.ObjectKey{Name: podName, Namespace: podNamespace}, &existing)
+		if errors.IsNotFound(err) {
+			m.logger.Debugf(ctx, "creating pod for node %s", podName)
+
+			// Create pod definition.
+			var pod corev1.Pod
+			{
+				command := `
 ([ -f /host/etc/kubernetes/manifests/k8s-controller-manager.yaml ] && mv /host/etc/kubernetes/manifests/k8s-controller-manager.yaml /host/root/) || true ;
 ([ -f /host/etc/kubernetes/manifests/k8s-api-server.yaml ] && mv /host/etc/kubernetes/manifests/k8s-api-server.yaml /host/root/) || true
 `
 
-			pod = corev1.Pod{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      podName,
-					Namespace: podNamespace,
-				},
-				Spec: corev1.PodSpec{
-					Volumes: []corev1.Volume{
-						{
-							Name: "host",
-							VolumeSource: corev1.VolumeSource{
-								HostPath: &corev1.HostPathVolumeSource{
-									Path: "/",
+				pod = corev1.Pod{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      podName,
+						Namespace: podNamespace,
+					},
+					Spec: corev1.PodSpec{
+						Volumes: []corev1.Volume{
+							{
+								Name: "host",
+								VolumeSource: corev1.VolumeSource{
+									HostPath: &corev1.HostPathVolumeSource{
+										Path: "/",
+									},
 								},
 							},
 						},
-					},
-					Containers: []corev1.Container{
-						{
-							Name:  "disable-master-node-components",
-							Image: "alpine:latest",
-							Command: []string{
-								"ash",
-								"-c",
-								command,
-							},
-							VolumeMounts: []corev1.VolumeMount{
-								{
-									Name:      "host",
-									ReadOnly:  false,
-									MountPath: "/host",
+						Containers: []corev1.Container{
+							{
+								Name:  "disable-master-node-components",
+								Image: "alpine:latest",
+								Command: []string{
+									"ash",
+									"-c",
+									command,
+								},
+								VolumeMounts: []corev1.VolumeMount{
+									{
+										Name:      "host",
+										ReadOnly:  false,
+										MountPath: "/host",
+									},
 								},
 							},
 						},
+						NodeName:           nodeName,
+						RestartPolicy:      corev1.RestartPolicyNever,
+						ServiceAccountName: serviceAccountName,
 					},
-					NodeName: nodeName,
-				},
+				}
 			}
-		}
 
-		// Create pod.
-		err = m.wcCtrlClient.Create(ctx, &pod)
-		if err != nil {
+			// Create pod.
+			err = m.wcCtrlClient.Create(ctx, &pod)
+			if err != nil {
+				return microerror.Mask(err)
+			}
+
+			m.logger.Debugf(ctx, "created pod for node %s", podName)
+			return nil
+		} else if err != nil {
 			return microerror.Mask(err)
 		}
+
+		m.logger.Debugf(ctx, "pod for node %s was already found", podName)
 	}
 
 	return nil
